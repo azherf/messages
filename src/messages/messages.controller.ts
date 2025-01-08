da24b6671586dd07bc3df8fcbd9f210d4ef0ed40
Azher Farooqi <azher@momentum.io>
2025-01-07 20:43:20 -0800

    chore: Use @Get @Post method decorators, and @Body, @Param argument decorators inside the messages controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'List all messages';
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+    return 'Create a message' + body;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+    return 'Get a message' + id;
+  }
+}
