5331622614c2eab0076d44000647f94327f86dcc
Azher Farooqi <azher@momentum.io>
2025-01-09 20:43:26 -0800

    chore: Add validation pipe and rules for content of POST request

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,9 +9,9 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
-    return 'Create a message' + body;
+    return 'Create a message ' + body.content;
   }
 
   @Get('/:id')
