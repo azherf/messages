5dbe6002571e770074b7cea3457796aed8593fa9
Azher Farooqi <azher@momentum.io>
2025-01-12 15:56:41 -0800

    feat: Create a messages.json to work as a repository. And create messages.repository file to get one, all and create messages logic

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string): Promise<{ id: string; content: string }> {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll(): Promise<{ id: string; content: string }[]> {
+    const contents = await readFile('messages.json', 'utf-8');
+    return JSON.parse(contents);
+  }
+
+  async create(message: string): Promise<{ id: string; content: string }> {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    const id = String(messages.length + 1);
+    const newMessage = { id, content: message };
+    messages[id] = newMessage;
+    await writeFile('messages.json', JSON.stringify(messages));
+    return newMessage;
+  }
+}
