b11f0d0ce261b6aa18ac6ad81e7acdcf36c18f60
Azher Farooqi <azher@momentum.io>
2025-01-13 20:26:03 -0800

    chore: Write functionality to create, read a single message, and read all messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,22 +1,33 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    //Don't do this is in real life
+    //Use dependency injeciton instead
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {
-    return 'List all messages';
+  listMessages(): Promise<{ id: string; content: string }[]> {
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
-    return 'Create a message ' + body.content;
+  createMessage(
+    @Body() body: CreateMessageDto,
+  ): Promise<{ id: string; content: string }> {
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
-    return 'Get a message' + id;
+  getMessage(
+    @Param('id') id: string,
+  ): Promise<{ id: string; content: string }> {
+    return this.messagesService.findOne(id);
   }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -15,7 +15,7 @@ export class MessagesRepository {
   async create(message: string): Promise<{ id: string; content: string }> {
     const contents = await readFile('messages.json', 'utf-8');
     const messages = JSON.parse(contents);
-    const id = String(messages.length + 1);
+    const id = String(Object.keys(messages).length + 1);
     const newMessage = { id, content: message };
     messages[id] = newMessage;
     await writeFile('messages.json', JSON.stringify(messages));
